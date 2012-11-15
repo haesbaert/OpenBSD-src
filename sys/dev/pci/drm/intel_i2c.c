bcc60292c75363d346455103bad8328aef25d0ef
Jonathan Gray <jsg@jsg.id.au>
2012-11-15 10:24:00 +0000

    gmbus i2c from kettenis

---
+++ sys/dev/pci/drm/intel_i2c.c
@@ -0,0 +1,130 @@
+#include "drmP.h"
+#include "drm.h"
+#include "i915_drv.h"
+#include "i915_reg.h"
+
+#include <dev/i2c/i2cvar.h>
+
+int	gmbus_i2c_acquire_bus(void *, int);
+void	gmbus_i2c_release_bus(void *, int);
+int	gmbus_i2c_exec(void *, i2c_op_t, i2c_addr_t, const void *, size_t,
+	    void *buf, size_t, int);
+
+struct gmbus_port {
+	struct inteldrm_softc *dev_priv;
+	int port;
+};
+
+int
+gmbus_i2c_acquire_bus(void *cookie, int flags)
+{
+	struct gmbus_port *gp = cookie;
+	struct inteldrm_softc *dev_priv = gp->dev_priv;
+
+	I915_WRITE(GMBUS0, GMBUS_RATE_100KHZ | gp->port);
+
+	return (0);
+}
+
+void
+gmbus_i2c_release_bus(void *cookie, int flags)
+{
+	struct gmbus_port *gp = cookie;
+	struct inteldrm_softc *dev_priv = gp->dev_priv;
+
+	I915_WRITE(GMBUS0, 0);
+}
+
+int
+gmbus_i2c_exec(void *cookie, i2c_op_t op, i2c_addr_t addr,
+    const void *cmdbuf, size_t cmdlen, void *buf, size_t len, int flags)
+{
+	struct gmbus_port *gp = cookie;
+	struct inteldrm_softc *dev_priv = gp->dev_priv;
+	uint32_t reg, st, val;
+	uint8_t *b;
+	int i, retries;
+
+	if (cmdlen > 1 || I2C_OP_WRITE_P(op))
+		return (EOPNOTSUPP);
+
+	reg = 0;
+	if (cmdlen > 0)
+		reg |= GMBUS_CYCLE_INDEX;
+	if (len > 0)
+		reg |= GMBUS_CYCLE_WAIT;
+	if (I2C_OP_STOP_P(op))
+		reg |= GMBUS_CYCLE_STOP;
+	if (I2C_OP_READ_P(op))
+		reg |= GMBUS_SLAVE_READ;
+	reg |= (addr << GMBUS_SLAVE_ADDR_SHIFT);
+	b = (void *)cmdbuf;
+	if (cmdlen > 0)
+		reg |= (b[0] << GMBUS_SLAVE_INDEX_SHIFT);
+	reg |= (len << GMBUS_BYTE_COUNT_SHIFT);
+	I915_WRITE(GMBUS1, reg | GMBUS_SW_RDY);
+
+	if (I2C_OP_READ_P(op)) {
+		b = buf;
+		while (len > 0) {
+			for (retries = 50; retries > 0; retries--) {
+				st = I915_READ(GMBUS2);
+				if (st & (GMBUS_SATOER | GMBUS_HW_RDY))
+					break;
+				DELAY(1000);
+			}
+			if (st & GMBUS_SATOER)
+				return (ENXIO);
+			if ((st & GMBUS_HW_RDY) == 0)
+				return (ETIMEDOUT);
+
+			val = I915_READ(GMBUS3);
+			for (i = 0; i < 4 && len > 0; i++, len--) {
+				*b++ = val & 0xff;
+				val >>= 8;
+			}
+		}
+	}
+
+	for (retries = 10; retries > 0; retries--) {
+		st = I915_READ(GMBUS2);
+		if ((st & GMBUS_ACTIVE) == 0)
+			break;
+		DELAY(1000);
+	}
+	if (st & GMBUS_ACTIVE)
+		return (ETIMEDOUT);
+
+	return (0);
+}
+
+void i915_i2c_probe(struct inteldrm_softc *);
+
+void
+i915_i2c_probe(struct inteldrm_softc *dev_priv)
+{
+	struct gmbus_port gp;
+	struct i2c_controller ic;
+	uint8_t buf[128];
+	uint8_t cmd;
+	int err, i;
+
+	gp.dev_priv = dev_priv;
+	gp.port = GMBUS_PORT_VGADDC;
+
+	ic.ic_cookie = &gp;
+	ic.ic_acquire_bus = gmbus_i2c_acquire_bus;
+	ic.ic_release_bus = gmbus_i2c_release_bus;
+	ic.ic_exec = gmbus_i2c_exec;
+
+	bzero(buf, sizeof(buf));
+	iic_acquire_bus(&ic, 0);
+	cmd = 0;
+	err = iic_exec(&ic, I2C_OP_READ_WITH_STOP, 0x50, &cmd, 1, buf, 128, 0);
+	if (err)
+		printf("err %d\n", err);
+	iic_release_bus(&ic, 0);
+	for (i = 0; i < sizeof(buf); i++)
+		printf(" 0x%02x", buf[i]);
+	printf("\n");
+}
