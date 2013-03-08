b53d592ff8e2b1fceb5009f11b59c40810f0eb4d
Jonathan Gray <jsg@jsg.id.au>
2013-03-08 14:06:35 +1100

    remove some no longer needed flush code

--- sys/dev/pci/drm/i915_gem_execbuffer.c
+++ sys/dev/pci/drm/i915_gem_execbuffer.c
@@ -450,24 +450,8 @@ i915_gem_execbuffer_retire_commands(struct drm_device *dev,
 				    struct drm_file *file,
 				    struct intel_ring_buffer *ring)
 {
-	u32 invalidate;
-
 	/* Unconditionally force add_request to emit a full flush. */
 	ring->gpu_caches_dirty = true;
-
-	/*
-	 * Ensure that the commands in the batch buffer are
-	 * finished before the interrupt fires.
-	 *
-	 * The sampler always gets flushed on i965 (sigh).
-	 */
-	invalidate = I915_GEM_DOMAIN_COMMAND;
-	if (INTEL_INFO(dev)->gen >= 4)
-		invalidate |= I915_GEM_DOMAIN_SAMPLER;
-	if (ring->flush(ring, invalidate, 0)) {
-		i915_gem_next_request_seqno(ring);
-		return;
-	}
 
 	/* Add a breadcrumb for the completion of the batch buffer */
 	if (i915_add_request(ring, file, NULL))
