1cac9ac0c9d879636c330b1ba8018b85450d9563
Jonathan Gray <jsg@jsg.id.au>
2013-03-12 01:47:44 +1100

    make the gen6+ ordering workaround conditional again

--- sys/dev/pci/drm/intel_ringbuffer.c
+++ sys/dev/pci/drm/intel_ringbuffer.c
@@ -797,10 +797,7 @@ gen6_ring_get_seqno(struct intel_ring_buffer *ring, bool lazy_coherency)
 	/* Workaround to force correct ordering between irq and seqno writes on
 	 * ivb (and maybe also on snb) by reading from a CS register (like
 	 * ACTHD) before reading the status page. */
-#ifdef notyet
-	/* XXX conditionally doing this seems to make everything laggy */
 	if (!lazy_coherency)
-#endif
 		intel_ring_get_active_head(ring);
 	return intel_read_status_page(ring, I915_GEM_HWS_INDEX);
 }
