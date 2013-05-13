142bfe41c0c7f19d9fe24380dc0f1a9a8df1ccf8
Jonathan Gray <jsg@jsg.id.au>
2013-05-13 15:01:03 +1000

    make radeon_connectors.c compile

--- sys/dev/pci/drm/radeon/radeon_connectors.c
+++ sys/dev/pci/drm/radeon/radeon_connectors.c
@@ -39,6 +39,11 @@ extern void
 radeon_atombios_connected_scratch_regs(struct drm_connector *connector,
 				       struct drm_encoder *encoder,
 				       bool connected);
+void	 radeon_add_atom_connector(struct drm_device *, uint32_t, uint32_t, int,
+	     struct radeon_i2c_bus_rec *, uint32_t, uint16_t, struct radeon_hpd *,
+	     struct radeon_router *);
+void	 radeon_add_legacy_connector(struct drm_device *, uint32_t, uint32_t, int,
+	     struct radeon_i2c_bus_rec *, uint16_t, struct radeon_hpd *);
 
 void radeon_connector_hotplug(struct drm_connector *connector)
 {
@@ -646,7 +651,9 @@ static void radeon_connector_destroy(struct drm_connector *connector)
 	if (radeon_connector->edid)
 		free(radeon_connector->edid, M_DRM);
 	free(radeon_connector->con_priv, M_DRM);
+#ifdef __linux__
 	drm_sysfs_connector_remove(connector);
+#endif
 	drm_connector_cleanup(connector);
 	free(connector, M_DRM);
 }
@@ -1214,7 +1221,9 @@ static void radeon_dp_connector_destroy(struct drm_connector *connector)
 	if (radeon_dig_connector->dp_i2c_bus)
 		radeon_i2c_destroy(radeon_dig_connector->dp_i2c_bus);
 	free(radeon_connector->con_priv, M_DRM);
+#ifdef __linux__
 	drm_sysfs_connector_remove(connector);
+#endif
 	drm_connector_cleanup(connector);
 	free(connector, M_DRM);
 }
@@ -1868,7 +1877,9 @@ radeon_add_atom_connector(struct drm_device *dev,
 		connector->polled = DRM_CONNECTOR_POLL_HPD;
 
 	connector->display_info.subpixel_order = subpixel_order;
+#ifdef __linux__
 	drm_sysfs_connector_add(connector);
+#endif
 	return;
 
 failed:
@@ -2026,5 +2037,7 @@ radeon_add_legacy_connector(struct drm_device *dev,
 	} else
 		connector->polled = DRM_CONNECTOR_POLL_HPD;
 	connector->display_info.subpixel_order = subpixel_order;
+#ifdef __linux__
 	drm_sysfs_connector_add(connector);
+#endif
 }
